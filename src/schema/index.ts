834b7f8056751d8b9baea7c346c85663bba81d2a
Keita Makino <kemakino@ucdavis.edu>
2024-06-27 12:24:44 -0700

    server merged into functions 19

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,8 +1,23 @@
 import { generateAllCrud } from "./generated/autocrud";
 import { builder } from "./builder";
 
-builder.queryType({});
-builder.mutationType({});
-generateAllCrud();
+builder.queryType({
+  description: "Root query type",
+  fields: (t) => ({
+    helloWorld: t.field({
+      type: "String",
+      resolve: () => "Hello, World!",
+    }),
+  }),
+});
+builder.mutationType({
+  description: "Root mutation type",
+  fields: (t) => ({
+    helloWorld: t.field({
+      type: "String",
+      resolve: () => "Hello, World!",
+    }),
+  }),
+});
 
 export const schema = builder.toSchema({});
