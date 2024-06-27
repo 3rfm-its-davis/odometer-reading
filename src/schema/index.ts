4a7c9b7a9c97f8810f6f030482721bf48a8060d9
Keita Makino <kemakino@ucdavis.edu>
2024-06-27 12:37:07 -0700

    server merged into functions 22

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -2,7 +2,7 @@ import { generateAllCrud } from "./generated/autocrud";
 import { builder } from "./builder";
 
 generateAllCrud();
-builder.queryType();
-builder.mutationType();
+builder.queryType({});
+builder.mutationType({});
 
 export const schema = builder.toSchema({});
