71d50566d2847abfcc385cf8565d00e5c34d27c1
Keita Makino <kemakino@ucdavis.edu>
2024-06-27 12:06:56 -0700

    server merged into functions 17

--- src/functions/database.ts
+++ src/functions/database.ts
@@ -3,21 +3,8 @@ import { v4 } from "@as-integrations/azure-functions";
 import { schema } from "../schema";
 import { app } from "@azure/functions";
 
-const typeDefs = `
-  type Query {
-    hello: String
-  }
-`;
-
-const resolvers = {
-  Query: {
-    hello: () => "world",
-  },
-};
-
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema,
 });
 
 app.http("graphql", {
--- src/schema/index.ts
+++ src/schema/index.ts
@@ -2,7 +2,23 @@ import { generateAllCrud } from "./generated/autocrud";
 import { builder } from "./builder";
 
 generateAllCrud();
-builder.queryType({});
-builder.mutationType({});
+builder.queryType({
+  description: "Root query type",
+  fields: (t) => ({
+    helloWorld: t.field({
+      type: "String",
+      resolve: () => "Hello, World!",
+    }),
+  }),
+});
+builder.mutationType({
+  description: "Root mutation type",
+  fields: (t) => ({
+    helloWorld: t.field({
+      type: "String",
+      resolve: () => "Hello, World!",
+    }),
+  }),
+});
 
 export const schema = builder.toSchema({});
