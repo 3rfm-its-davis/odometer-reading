79d777917bfc76be5ae63335c9dc62d58979fb80
Keita Makino <kemakino@ucdavis.edu>
2024-06-27 11:50:40 -0700

    server merged into functions 16

--- src/functions/database.ts
+++ src/functions/database.ts
@@ -3,12 +3,23 @@ import { v4 } from "@as-integrations/azure-functions";
 import { schema } from "../schema";
 import { app } from "@azure/functions";
 
+const typeDefs = `
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "world",
+  },
+};
+
 const server = new ApolloServer({
-  schema,
+  typeDefs,
+  resolvers,
 });
 
 app.http("graphql", {
-  methods: ["POST"],
-  authLevel: "anonymous",
   handler: v4.startServerAndCreateHandler(server),
 });
