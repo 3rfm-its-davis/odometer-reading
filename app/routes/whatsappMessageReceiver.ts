247b0e577119dda555a78c176cd87a632a379bbc
Keita Makino <kemakino@ucdavis.edu>
2024-07-07 19:23:38 -0700

    remix webhook

--- app/routes/whatsappMessageReceiver.ts
+++ app/routes/whatsappMessageReceiver.ts
@@ -1,31 +1,28 @@
-import crypto from "node:crypto";
-
 import type { ActionFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 
-export const action = async ({ request }: ActionFunctionArgs) => {
-  if (request.method !== "POST") {
-    return json({ message: "Method not allowed" }, 405);
+export const action = async ({ request, params }: ActionFunctionArgs) => {
+  if (params["hub.mode"] !== "subscribe") {
+    if (request.method !== "POST") {
+      return json({ message: "Method not allowed" }, 405);
+    }
+
+    const secret = process.env.WHATSAPP_WEBHOOK_SECRET;
+
+    if (!secret) {
+      return json({ message: "Secret not set" }, 500);
+    }
+
+    if (!request.body) {
+      return json({ message: "No body provided" }, 400);
+    }
+    const verifyToken = params["hub.verify_token"];
+
+    if (secret !== verifyToken) {
+      return json({ message: "Verify token mismatch" }, 401);
+    }
+
+    return json({ body: params["hub.challenge"] }, 200);
   }
-
-  const secret = process.env.WHATSAPP_WEBHOOK_SECRET;
-
-  if (!secret) {
-    return json({ message: "Secret not set" }, 500);
-  }
-
-  if (!request.body) {
-    return json({ message: "No body provided" }, 400);
-  }
-
-  const bodyData = await request.text();
-  const parsedBody = JSON.parse(bodyData);
-  console.log("Parsed body: ", JSON.stringify(parsedBody));
-  const verifyToken = parsedBody["hub.verify_token"];
-
-  if (secret !== verifyToken) {
-    return json({ message: "Verify token mismatch" }, 401);
-  }
-
-  return json({ body: parsedBody["hub.challenge"] }, 200);
+  return json({ message: "Invalid request" }, 400);
 };
