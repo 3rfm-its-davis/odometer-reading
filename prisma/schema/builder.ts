ecea5b865ad774aa9020e8813cdf70b74414d324
Keita Makino <kemakino@ucdavis.edu>
2024-07-07 18:09:34 -0700

    remix 2

--- prisma/schema/builder.ts
+++ prisma/schema/builder.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 import PrismaPlugin from "@pothos/plugin-prisma";
 import type PrismaTypes from "@pothos/plugin-prisma/generated";
 
-const prisma = new PrismaClient();
+export const prisma = new PrismaClient();
 export const builder = new SchemaBuilder<{
   Scalars: {
     DateTime: {
