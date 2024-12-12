d727d01056ed302361ac6d55cfcb628e804d281e
Keita Makino <kemakino@ucdavis.edu>
2024-12-12 14:24:08 -0800

    cronjob to retrieve qualtrics responses

--- app/server/cron.server.ts
+++ app/server/cron.server.ts
@@ -1,5 +1,10 @@
+import axios from "axios";
 import { CronJob } from "cron";
+import dotenv from "dotenv";
 import { prisma } from "./prisma.server";
+import { RegisterUser } from "./registerUsers";
+
+dotenv.config();
 
 export const cronJob = async () => {
   console.log("Cronjob started");
@@ -36,6 +41,103 @@ export const cronJob = async () => {
         .then(() => {
           console.log(`Cronjob complete: ${users.length} users closed`);
         });
+    },
+    start: true,
+    timeZone: "America/Los_Angeles",
+  });
+
+  CronJob.from({
+    cronTime: "0 */10 * * * *",
+    onTick: async () => {
+      const continuationToken = (
+        await prisma.lastQualtricsResponseRetrieval.findFirst({})
+      )?.id;
+
+      const progressId = (
+        await axios.post(
+          `https://${process.env.QUALTRICS_SERVER}.qualtrics.com/API/v3/surveys/${process.env.QUALTRICS_SURVEY}/export-responses`,
+          {
+            format: "json",
+            limit: 5,
+            compress: false,
+            allowContinuation: continuationToken !== "default" ? false : true,
+            continuationToken:
+              continuationToken !== "default" ? continuationToken : undefined,
+          },
+          {
+            headers: {
+              "X-API-TOKEN": process.env.QUALTRICS_API_KEY,
+            },
+          }
+        )
+      ).data?.result?.progressId;
+
+      if (!progressId) {
+        return;
+      }
+
+      let fileId = undefined;
+      let newContinuationToken = continuationToken;
+
+      const getFileIdInterval = setInterval(async () => {
+        const response = await axios.get(
+          `https://${process.env.QUALTRICS_SERVER}.qualtrics.com/API/v3/surveys/${process.env.QUALTRICS_SURVEY}/export-responses/${progressId}`,
+          {
+            headers: {
+              "X-API-TOKEN": process.env.QUALTRICS_API_KEY,
+            },
+          }
+        );
+
+        fileId = response.data?.result?.fileId;
+        newContinuationToken = response.data?.result?.continuationToken;
+
+        if (fileId) {
+          clearInterval(getFileIdInterval);
+        }
+      }, 2500);
+
+      while (!fileId) {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+      }
+
+      const file = (
+        await axios.get(
+          `https://${process.env.QUALTRICS_SERVER}.qualtrics.com/API/v3/surveys/${process.env.QUALTRICS_SURVEY}/export-responses/${fileId}/file`,
+          {
+            headers: {
+              "X-API-TOKEN": process.env.QUALTRICS_API_KEY,
+            },
+          }
+        )
+      ).data;
+
+      if (file.responses.length > 0) {
+        await prisma.lastQualtricsResponseRetrieval.update({
+          where: {
+            id: continuationToken,
+          },
+          data: {
+            id: newContinuationToken,
+          },
+        });
+      }
+
+      const emailsRetrieved: string[] = file.responses
+        .map((item: { values: { QID9_1: any } }) => item.values.QID9_1)
+        .filter((item: string) => item !== undefined)
+        .filter(
+          (item: string, index: number, array: string[]) =>
+            array.indexOf(item) === index
+        );
+
+      if (emailsRetrieved.length === 0) {
+        return;
+      }
+
+      RegisterUser(emailsRetrieved);
+
+      return;
     },
     start: true,
     timeZone: "America/Los_Angeles",
